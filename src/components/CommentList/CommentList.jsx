32d7611c2ecc412854488058e6352d6b104b9809
Carlos Alberto <betto.1993@hotmaail.com>
2026-07-13 23:09:24 -0300

    add task solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,13 @@
-export const CommentList = () => <>Put the list here</>;
+import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo';
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {comments.length === 0 ? (
+      <div data-cy="NoCommentsMessage">There are no comments yet</div>
+    ) : (
+      comments.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))
+    )}
+  </div>
+);
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,14 @@
-export const PostList = () => <>Put the list here</>;
+import './PostList.scss';
+import { PostInfo } from '../PostInfo';
+export const PostList = ({ posts, comments, users }) => (
+  <div className="PostList">
+    {posts.map((post) => {
+      const postUser = users.find(u => u.id === post.userId);
+      if (!postUser) return null;
+      return (
+        <PostInfo key={post.id} post={post} comments={comments} user={postUser} />
+      );
+    })}
+  </div>
+);
+
